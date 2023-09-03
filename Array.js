5bbe93e129289c818c90f9b50105b35ef78374da
Sonali khosle <142083474+SonaliK99@users.noreply.github.com>
2023-09-03 13:51:09 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,18 @@
+
+//Array
+// Heterogenous array
+let cars = ['Sonali', 'Khosle', '100', 'Audi', true, 200, test()];
+
+function test() {
+    return 20;
+}
+console.log(cars);
+console.log(cars.length);
+console.log(cars[1]);
+
+
+//Homogenous array
+let car = ['sonali', 'khosle', 'mca', 'Audi'];
+console.log(car.length);
+console.log(car[2]);
+
